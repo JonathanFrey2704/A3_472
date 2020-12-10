016a2c146403de37c278c136a725e61b57318f9c
JonathanFrey2704 <jfrey2704@gmail.com>
2020-12-09 19:37:40 -0500

    .

--- main.py
+++ main.py
@@ -3,20 +3,37 @@ import string
 from nltk.corpus import stopwords
 
 if __name__ == '__main__':
+
+    #read data
     tsv_file = open("covid_training.tsv")
     read_tsv = csv.reader(tsv_file, delimiter="\t")
     next(read_tsv) #ignore header
 
-
-    list_of_tweets = []
+    tweet_ids = []
+    tweets = []
+    y = []
     for row in read_tsv:
-        list_of_tweets.append(row[1].lower())
+        tweet_ids.append(row[0])
+        tweets.append(row[1].lower())
+        y.append(row[2])
+    #print(tweets)
 
-    tokenized_tweets = []
-    for tweet in list_of_tweets:
+    #tokenize tweets, remove punctuation and stopwords
+    tokenized_tweets =[]
+    stopwords = set(stopwords.words('english'))
+    for tweet in tweets:
         temp = [char for char in tweet if char not in string.punctuation]
         temp = ''.join(temp)
-        tokenized_tweets.append(temp.split())
-    print(tokenized_tweets)
+        tokenized_tweets.append([word for word in temp.split() if word not in stopwords])
+    #print(tokenized_tweets)
 
+    vocabulary ={}
+    for tokenized_tweet in tokenized_tweets:
+        for word in tokenized_tweet:
+            if word not in vocabulary:
+                vocabulary[word] = 1
+            else:
+                vocabulary[word] += 1
+
+    #print(vocabulary)
 
